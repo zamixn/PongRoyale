f011e61406ecee39059aab2daaf29ddc2dc37076
erimac2 <mackevicius.erikas1@gmail.com>
2020-10-11 23:11:55 +0300

    Abstract factory implementation

---
+++ PongRoyale_client/PongRoyale_client/Game/Obstacles/ObstacleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongRoyale_client.Game.Obstacles
+{
+    public enum ObstacleType
+    {
+        yes = 1,
+        no = 2
+    }
+}
